69449a570055d15dc19c08f17c1f2ad590505ba9
majunyi <732231129@qq.com>
2020-08-15 18:28:11 +0800

    swagger测试提交

--- jpa-swagger-feign/src/main/java/com/systemstu/util/SwaggerConfig.java
+++ jpa-swagger-feign/src/main/java/com/systemstu/util/SwaggerConfig.java
@@ -40,7 +40,6 @@ public class SwaggerConfig {
     /**
      * 创建该API的基本信息（这些基本信息会展现在文档页面中）
      * 访问地址：http://项目实际地址/swagger-ui.html
-     *
      * @return
      */
     private ApiInfo apiInfo() {
